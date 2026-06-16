4b18c71539695d34595fdc08fba7301eb49127a2
Usuario invitado <Guest@iMac-de-PUCE-A10814.local>
2026-06-16 17:35:20 -0500

    16/06/2026

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,5 +1,15 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+  IonCard,
+  IonCardContent,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from '@ionic/react';
+
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
@@ -10,13 +20,35 @@ const Tab3: React.FC = () => {
           <IonTitle>Tab 3</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 3</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+
+        <div className="card-container">
+          <IonCard className="card">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlRu0l-I-c4ZMqtetMnKbBnTChk_bj3aqfCg&s"
+              alt="Avatar"
+            />
+
+            <IonHeader>
+              <IonCardTitle>Eduardo Céspedes</IonCardTitle>
+              <IonCardSubtitle>@eduardocespedes</IonCardSubtitle>
+            </IonHeader>
+
+            <IonCardContent>
+              <p>
+                Desarrollador de software con experiencia en aplicaciones
+                móviles y web. Apasionado por la tecnología y el aprendizaje
+                continuo.
+              </p>
+            </IonCardContent>
+          </IonCard>
+        </div>
       </IonContent>
     </IonPage>
   );
